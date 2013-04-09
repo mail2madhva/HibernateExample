7feaf45bc71efe97aa84e07f31fb515b4d9cd695
madhu <mail2madhva@gmail.com>
2013-04-09 16:38:57 +0530

    Create a table using hibernate -part 1 of the tutorial

---
+++ src/com/hib/part1/TestUser.java
@@ -0,0 +1,32 @@
+package com.hib.part1;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.hibernate.Session;
+
+public class TestUser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		AnnotationConfiguration config = new AnnotationConfiguration();
+		
+		config.addAnnotatedClass(User.class);
+		config.configure();
+		
+		new SchemaExport(config).create(true, true);
+		
+//		SessionFactory factory = config.buildSessionFactory();
+//		Session session = factory.getCurrentSession();
+//		
+//		
+//		User first = new User();
+//		first.setUser_id(1);
+//		first.setUser_name("Madhu");
+		
+	}
+
+}
---
+++ src/com/hib/part1/User.java
@@ -0,0 +1,26 @@
+package com.hib.part1;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="User")
+public class User {
+	
+	private int user_id;
+	private String user_name;
+	
+	@Id
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+}
